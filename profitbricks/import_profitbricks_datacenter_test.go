0caa99b6c81953ae47a3973412d60aff52f8f4fe
Christian Groschupp <christian@groschupp.org>
2018-07-06 14:15:44 +0200

    Add importer to datacenter

---
+++ profitbricks/import_profitbricks_datacenter_test.go
@@ -0,0 +1,28 @@
+package profitbricks
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"testing"
+)
+
+func TestAccProfitBricksDataCenter_ImportBasic(t *testing.T) {
+	resourceName := "datacenter-importtest"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDProfitBricksDatacenterDestroyCheck,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: fmt.Sprintf(testAccCheckProfitBricksDatacenterConfig_basic, resourceName),
+			},
+
+			resource.TestStep{
+				ResourceName:      fmt.Sprintf("profitbricks_datacenter.foobar"),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
