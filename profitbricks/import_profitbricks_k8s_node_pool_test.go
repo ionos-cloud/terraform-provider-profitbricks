fd6036d8cfce8db4985532be22eebbd68221dc8e
Alexandru Bogdan Pica <alexandru.pica@ionos.com>
2020-05-23 11:28:41 +0300

    Updates: Node Pool Import test updates

--- profitbricks/import_profitbricks_k8s_node_pool_test.go
+++ profitbricks/import_profitbricks_k8s_node_pool_test.go
@@ -13,13 +13,13 @@ func TestAccProfitBricksk8sNodepool_ImportBasic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckDProfitBricksk8sClusterDestroyCheck,
+		CheckDestroy: testAccCheckDProfitBricksk8sNodepoolDestroyCheck,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(testAccCheckProfitBricksk8sClusterConfigBasic, resourceName),
+				Config: fmt.Sprintf(testAccCheckProfitBricksk8sNodepoolConfigBasic, resourceName),
 			},
 			{
-				ResourceName:      fmt.Sprintf("profitbricks_k8s_cluster.%s", resourceName),
+				ResourceName:      fmt.Sprintf("profitbricks_k8s_node_pool.%s", resourceName),
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
