de7208507ec35c5d087f4e1ebb90c404870cbfa7
Alexandru Bogdan Pica <alexandru.pica@ionos.com>
2020-05-24 11:28:11 +0300

    Updates: added ImportStateIdFunc to the node pool import test

--- go.mod
+++ go.mod
@@ -40,7 +40,7 @@ require (
 	github.com/zclconf/go-cty v1.4.1 // indirect
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
 	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
-	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
 	golang.org/x/tools v0.0.0-20200522201501-cb1345f3a375 // indirect
 	google.golang.org/api v0.25.0 // indirect
 	google.golang.org/genproto v0.0.0-20200521103424-e9a78aa275b7 // indirect
--- profitbricks/import_profitbricks_k8s_node_pool_test.go
+++ profitbricks/import_profitbricks_k8s_node_pool_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccProfitBricksk8sNodepool_ImportBasic(t *testing.T) {
@@ -20,9 +21,24 @@ func TestAccProfitBricksk8sNodepool_ImportBasic(t *testing.T) {
 			},
 			{
 				ResourceName:      fmt.Sprintf("profitbricks_k8s_node_pool.%s", resourceName),
+				ImportStateIdFunc: testAccProfitBricksk8sNodepoolImportStateID,
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 		},
 	})
+}
+
+func testAccProfitBricksk8sNodepoolImportStateID(s *terraform.State) (string, error) {
+	var importID string = ""
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "profitbricks_k8s_node_pool" {
+			continue
+		}
+
+		importID = fmt.Sprintf("%s/%s", rs.Primary.Attributes["k8s_cluster_id"], rs.Primary.ID)
+	}
+
+	return importID, nil
 }
